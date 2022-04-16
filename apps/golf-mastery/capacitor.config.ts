1a08f8c55559d16728c5a4851c9fd7ee9c824180
GolferGeek <golfergeek@gmail.com>
2022-04-16 16:49:41 -0500

    just getting started

---
+++ apps/golf-mastery/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'golf-mastery',
+  webDir: '../../dist/apps/golf-mastery',
+  bundledWebRuntime: false,
+};
+
+export default config;
